52468d3c8d8542753828e9db5adea70353b054ec
lvivas-77 <1hippito@gmail.com>
2026-09-03 16:41:18 -0500

    Update import paths for builderbot modules

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-import bot from '@builderbot/bot'
-const { createBot, createProvider, createFlow, addKeyword } = bot
-import BaileysProvider from '@builderbot/bot/dist/providers/baileys/index.cjs'
-import MemoryDB from '@builderbot/bot/dist/databases/memory/index.cjs'
+import { createBot, createProvider, createFlow, addKeyword } from '@builderbot/bot'
+import BaileysProvider from '@builderbot/bot/providers/baileys/index.js'
+import MemoryDB from '@builderbot/bot/databases/memory/index.js'
 
 const URL_ASESOR = 'https://whatsapp.com'
 
